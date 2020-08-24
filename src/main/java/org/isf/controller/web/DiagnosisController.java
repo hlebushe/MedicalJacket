e1e88c2d8113cd32c7384884db38e2aff759553f
andreyblonskiy <andrey.blonskiy@solid-apps.de>
2020-08-24 22:19:27 +0300

    Revert "Revert "added api medic""
    This reverts commit 36d55c29b04a05dcee32a1e902fdfd887354ce5c.

--- src/main/java/org/isf/controller/web/DiagnosisController.java
+++ src/main/java/org/isf/controller/web/DiagnosisController.java
@@ -1,5 +1,8 @@
 package org.isf.controller.web;
 
+import org.isf.priaid.Diagnosis.Model.Gender;
+import org.isf.priaid.Diagnosis.Model.HealthDiagnosis;
+import org.isf.service.PriaidService;
 import org.isf.service.XLSXService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +14,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RequestMapping(value = "/diagnosis")
 
@@ -19,18 +25,6 @@ public class DiagnosisController {
 
     @Autowired
     XLSXService xlsxService;
-
-    @RequestMapping(value = "/get_symptom_info", method = RequestMethod.GET)
-    public @ResponseBody String getSymptomInfo(@RequestParam String diagnosis1, @RequestParam String infoType, HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String res = "";
-
-        if (!diagnosis1.isEmpty()) {
-            res = res.concat(xlsxService.getSymptomInfo(diagnosis1, infoType));
-        }
-
-        return res;
-    }
-
 
     @RequestMapping(value = "/get_symptoms_info", method = RequestMethod.GET)
     public @ResponseBody String getSymptomsInfo(@RequestParam String diagnosis1, @RequestParam String diagnosis2, @RequestParam String diagnosis3, @RequestParam String infoType, HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -50,6 +44,47 @@ public class DiagnosisController {
 
         return res;
     }
+
+    @RequestMapping(value = "/get_diagnosis", method = RequestMethod.GET)
+    public @ResponseBody String getDiagnosisInfo(@RequestParam String symptom1, @RequestParam String symptom2,
+                                                 @RequestParam String symptom3, @RequestParam String symptom4, @RequestParam String symptom5,
+                                                 @RequestParam String yearOfBirth, @RequestParam String sex,
+                                                 HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String res = "";
+
+        PriaidService priaidService = new PriaidService();
+        priaidService.setClient();
+
+        List<Integer> symptoms = new ArrayList<>();
+        symptoms.add(Integer.valueOf(symptom1));
+        symptoms.add(Integer.valueOf(symptom2));
+        symptoms.add(Integer.valueOf(symptom3));
+        symptoms.add(Integer.valueOf(symptom4));
+        symptoms.add(Integer.valueOf(symptom5));
+
+        symptoms.removeAll(Arrays.asList(0));
+
+        Gender gender = null;
+
+        if (sex.equals("M")) {
+            gender = Gender.Male;
+        } else {
+            gender = Gender.Female;
+        }
+
+        List<HealthDiagnosis> healthDiagnoses = priaidService.getDiagnosisBySymptoms(symptoms, gender, Integer.parseInt(yearOfBirth));
+
+        if (healthDiagnoses.isEmpty()) {
+            res = "No diagnosis";
+        }
+
+        for (HealthDiagnosis healthDiagnosis : healthDiagnoses) {
+            res = res.concat(healthDiagnosis.Issue.IcdName + " - " + healthDiagnosis.Issue.Name + System.lineSeparator());
+        }
+
+        return res;
+    }
+
 
 }
 
