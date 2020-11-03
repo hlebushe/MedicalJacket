f7dfe5a4c5b8952e31065e7894d523e46be5f0be
andreyblonskiy <andrey.blonskiy@solid-apps.de>
2020-11-03 19:10:03 +0200

    updated report generator

--- src/main/java/org/isf/service/PDFService.java
+++ src/main/java/org/isf/service/PDFService.java
@@ -8,6 +8,7 @@ import com.google.zxing.qrcode.QRCodeWriter;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
@@ -17,8 +18,13 @@ import org.isf.util.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.vandeseer.easytable.TableDrawer;
+import org.vandeseer.easytable.structure.Row;
+import org.vandeseer.easytable.structure.Table;
+import org.vandeseer.easytable.structure.cell.TextCell;
 
 import javax.servlet.ServletContext;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystems;
@@ -29,6 +35,9 @@ import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static org.vandeseer.easytable.settings.HorizontalAlignment.CENTER;
+import static org.vandeseer.easytable.settings.HorizontalAlignment.LEFT;
 
 @Service
 public class PDFService {
@@ -52,59 +61,28 @@ public class PDFService {
             document.addPage(page);
             PDPageContentStream contentStream = new PDPageContentStream(document, page);
 
+            PDRectangle mediabox = page.getMediaBox();
+            float heightCounter = 574;
 
             addHeader(contentStream, visit, page);
             addPatientData(contentStream, visit, doctor, document);
-            addSymptomsAndDiagnosis(contentStream, visit);
+            heightCounter = addSymptomsAndDiagnosis(contentStream, visit, heightCounter);
+            heightCounter = addMedications(contentStream, visit, heightCounter);
 
-            if (!visit.getMedication1().equals("")) {
-                contentStream.showText("Prescribed Medication: ");
-            }
-            if (!visit.getMedication1().equals("")) {
-                contentStream.newLine();
-                contentStream.showText("- " + visit.getMed1Name() + " (" + visit.getMed1Time() + ") for " + visit.getMed1Duration() + "; (" + visit.getMed1Caution() + ")");
-            }
-            if (!visit.getMedication2().equals("")) {
-                contentStream.newLine();
-                contentStream.showText("- " + visit.getMed2Name() + " (" + visit.getMed2Time() + ") for " + visit.getMed2Duration() + "; (" + visit.getMed2Caution() + ")");
-            }
-            if (!visit.getMedication3().equals("")) {
-                contentStream.newLine();
-                contentStream.showText("- " + visit.getMed3Name() + " (" + visit.getMed3Time() + ") for " + visit.getMed3Duration() + "; (" + visit.getMed3Caution() + ")");
-            }
-            if (!visit.getMedication4().equals("")) {
-                contentStream.newLine();
-                contentStream.showText("- " + visit.getMed4Name() + " (" + visit.getMed4Time() + ") for " + visit.getMed4Duration() + "; (" + visit.getMed4Caution() + ")");
-            }
-            if (!visit.getMedication5().equals("")) {
-                contentStream.newLine();
-                contentStream.showText("- " + visit.getMed5Name() + " (" + visit.getMed5Time() + ") for " + visit.getMed5Duration() + "; (" + visit.getMed5Caution() + ")");
-            }
-            if (!visit.getMedication6().equals("")) {
-                contentStream.newLine();
-                contentStream.showText("- " + visit.getMed6Name() + " (" + visit.getMed6Time() + ") for " + visit.getMed6Duration() + "; (" + visit.getMed6Caution() + ")");
+
+            if (heightCounter < 200) {
+                contentStream.close();
+                PDPage page2 = new PDPage();
+                document.addPage(page2);
+                contentStream = new PDPageContentStream(document, page2);
+                heightCounter = 670;
             }
 
-            contentStream.newLine();
-            contentStream.newLine();
+            addExaminations(contentStream, visit, heightCounter);
+            addFooter(contentStream, visit);
 
-            if (!visit.getExaminationsPrescribed().equals("")) contentStream.showText("Examinations Prescribed: " + visit.getExaminationsPrescribed());
-
-            contentStream.newLine();
-            contentStream.newLine();
-
-            if (visit.getNextVisitDate() != null) {
-                String date = visit.getNextVisitDate().toString().substring(0, 10);
-                String formattedDate = DateUtil.format(date);
-                contentStream.showText("Next Visit Date: " + formattedDate);
-            }
-
-            contentStream.newLine();
-            contentStream.newLine();
             contentStream.endText();
             contentStream.close();
-
-
 
             document.save(mContext.getRealPath("/") + visit.getVisitID() + ".pdf");
             document.close();
@@ -187,6 +165,7 @@ public class PDFService {
             contentStream.newLine();
             contentStream.showText("Age: " + visit.getPatient().getAge() + "       Gender: " + visit.getPatient().getSex());
             contentStream.newLine();
+            contentStream.newLine();
 
             contentStream.setFont(PDType1Font.TIMES_ROMAN, 12);
             contentStream.showText("Patient Telephone Number: " + visit.getPatient().getTelephone());
@@ -201,13 +180,13 @@ public class PDFService {
             PDImageXObject QRImage = PDImageXObject.createFromFile(qr, document);
             contentStream.endText();
 
-            contentStream.drawImage(QRImage, 400, 620);
+            contentStream.drawImage(QRImage, 400, 575);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void addSymptomsAndDiagnosis(PDPageContentStream contentStream, Visit visit) {
+    public float addSymptomsAndDiagnosis(PDPageContentStream contentStream, Visit visit, float heightCounter) {
         try{
             String mainCompSympt = visit.getMainComplaintSymptom();
             List<String> symptomsList = new ArrayList<>();
@@ -222,20 +201,23 @@ public class PDFService {
 
             contentStream.beginText();
             contentStream.setLeading(17f);
-            contentStream.newLineAtOffset(50, 520);
+            contentStream.newLineAtOffset(50, 500);
             contentStream.setFont(PDType1Font.TIMES_BOLD, 12);
             contentStream.showText("Symptoms: ");
             contentStream.setFont(PDType1Font.TIMES_ROMAN, 12);
             contentStream.newLine();
+            heightCounter = heightCounter - 24;
 
             if (mainCompSympt.equals("")) {
                 contentStream.showText("no symptoms");
                 contentStream.newLine();
+                heightCounter = heightCounter - 12;
             } else {
                 int index = 1;
                 for (String symptom : symptomsList) {
                     contentStream.showText((index++) + "." + symptom);
                     contentStream.newLine();
+                    heightCounter = heightCounter - 12;
                 }
             }
 
@@ -244,24 +226,246 @@ public class PDFService {
             contentStream.showText("Diagnosis: ");
             contentStream.setFont(PDType1Font.TIMES_ROMAN, 12);
             contentStream.newLine();
+            heightCounter = heightCounter - 24;
 
             if (mainCompDia.equals("")) {
                 contentStream.showText("no diagnosis");
                 contentStream.newLine();
+                heightCounter = heightCounter - 12;
             } else {
                 int index = 1;
                 for (String diagnosis : diagnosisList) {
                     contentStream.showText((index++) + "." + diagnosis);
                     contentStream.newLine();
+                    heightCounter = heightCounter - 12;
                 }
             }
+            contentStream.newLine();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return heightCounter;
     }
 
-    public void addMedications(PDPageContentStream contentStream, Visit visit) {
+    public float addMedications(PDPageContentStream contentStream, Visit visit, float heightCounter) {
+        float parallelCounter = heightCounter;
+        try {
+            if (!visit.getMedication1().equals("")) {
+                contentStream.setFont(PDType1Font.TIMES_BOLD, 12);
+                contentStream.showText("Prescribed Medication: ");
+            }
+
+            heightCounter = heightCounter - 24;
+            parallelCounter = parallelCounter - 48;
+
+            final Table.TableBuilder tableBuilder = Table.builder()
+                    .addColumnsOfWidth(175, 70, 70, 150)
+                    .fontSize(10)
+                    .font(PDType1Font.TIMES_ROMAN)
+                    .borderColor(Color.BLACK);
+
+            tableBuilder.addRow(Row.builder()
+                    .add(TextCell.builder().text("Name of Medicine").horizontalAlignment(LEFT).borderWidth(1).build())
+                    .add(TextCell.builder().text("Time of use").borderWidth(1).build())
+                    .add(TextCell.builder().text("Duration").borderWidth(1).build())
+                    .add(TextCell.builder().text("Notes").borderWidth(1).build())
+                    .backgroundColor(Color.WHITE)
+                    .textColor(Color.BLACK)
+                    .font(PDType1Font.TIMES_BOLD)
+                    .fontSize(12)
+                    .horizontalAlignment(CENTER)
+                    .build());
+
+            heightCounter = heightCounter - 24;
+            parallelCounter = parallelCounter - 48;
+
+            if (!visit.getMedication1().equals("")) {
+                tableBuilder.addRow(Row.builder()
+                        .add(TextCell.builder().text(visit.getMed1Name()).horizontalAlignment(LEFT).borderWidth(1).build())
+                        .add(TextCell.builder().text(visit.getMed1Time()).borderWidth(1).build())
+                        .add(TextCell.builder().text(visit.getMed1Duration()).borderWidth(1).build())
+                        .add(TextCell.builder().text(visit.getMed1Caution()).borderWidth(1).build())
+                        .backgroundColor(Color.WHITE)
+                        .textColor(Color.BLACK)
+                        .font(PDType1Font.TIMES_ROMAN)
+                        .fontSize(10)
+                        .horizontalAlignment(CENTER)
+                        .build());
+                heightCounter = heightCounter - 24;
+                parallelCounter = parallelCounter - 48;
+            }
+            if (!visit.getMedication2().equals("")) {
+                tableBuilder.addRow(Row.builder()
+                        .add(TextCell.builder().text(visit.getMed2Name()).horizontalAlignment(LEFT).borderWidth(1).build())
+                        .add(TextCell.builder().text(visit.getMed2Time()).borderWidth(1).build())
+                        .add(TextCell.builder().text(visit.getMed2Duration()).borderWidth(1).build())
+                        .add(TextCell.builder().text(visit.getMed2Caution()).borderWidth(1).build())
+                        .backgroundColor(Color.WHITE)
+                        .textColor(Color.BLACK)
+                        .font(PDType1Font.TIMES_ROMAN)
+                        .fontSize(10)
+                        .horizontalAlignment(CENTER)
+                        .build());
+                heightCounter = heightCounter - 24;
+                parallelCounter = parallelCounter - 48;
+            }
+            if (!visit.getMedication3().equals("")) {
+                tableBuilder.addRow(Row.builder()
+                        .add(TextCell.builder().text(visit.getMed3Name()).horizontalAlignment(LEFT).borderWidth(1).build())
+                        .add(TextCell.builder().text(visit.getMed3Time()).borderWidth(1).build())
+                        .add(TextCell.builder().text(visit.getMed3Duration()).borderWidth(1).build())
+                        .add(TextCell.builder().text(visit.getMed3Caution()).borderWidth(1).build())
+                        .backgroundColor(Color.WHITE)
+                        .textColor(Color.BLACK)
+                        .font(PDType1Font.TIMES_ROMAN)
+                        .fontSize(10)
+                        .horizontalAlignment(CENTER)
+                        .build());
+                heightCounter = heightCounter - 24;
+                parallelCounter = parallelCounter - 48;
+            }
+            if (!visit.getMedication4().equals("")) {
+                tableBuilder.addRow(Row.builder()
+                        .add(TextCell.builder().text(visit.getMed4Name()).horizontalAlignment(LEFT).borderWidth(1).build())
+                        .add(TextCell.builder().text(visit.getMed4Time()).borderWidth(1).build())
+                        .add(TextCell.builder().text(visit.getMed4Duration()).borderWidth(1).build())
+                        .add(TextCell.builder().text(visit.getMed4Caution()).borderWidth(1).build())
+                        .backgroundColor(Color.WHITE)
+                        .textColor(Color.BLACK)
+                        .font(PDType1Font.TIMES_ROMAN)
+                        .fontSize(10)
+                        .horizontalAlignment(CENTER)
+                        .build());
+                heightCounter = heightCounter - 24;
+                parallelCounter = parallelCounter - 48;
+            }
+            if (!visit.getMedication5().equals("")) {
+                tableBuilder.addRow(Row.builder()
+                        .add(TextCell.builder().text(visit.getMed5Name()).horizontalAlignment(LEFT).borderWidth(1).build())
+                        .add(TextCell.builder().text(visit.getMed5Time()).borderWidth(1).build())
+                        .add(TextCell.builder().text(visit.getMed5Duration()).borderWidth(1).build())
+                        .add(TextCell.builder().text(visit.getMed5Caution()).borderWidth(1).build())
+                        .backgroundColor(Color.WHITE)
+                        .textColor(Color.BLACK)
+                        .font(PDType1Font.TIMES_ROMAN)
+                        .fontSize(10)
+                        .horizontalAlignment(CENTER)
+                        .build());
+                heightCounter = heightCounter - 24;
+                parallelCounter = parallelCounter - 48;
+            }
+            if (!visit.getMedication6().equals("")) {
+                tableBuilder.addRow(Row.builder()
+                        .add(TextCell.builder().text(visit.getMed6Name()).horizontalAlignment(LEFT).borderWidth(1).build())
+                        .add(TextCell.builder().text(visit.getMed6Time()).borderWidth(1).build())
+                        .add(TextCell.builder().text(visit.getMed6Duration()).borderWidth(1).build())
+                        .add(TextCell.builder().text(visit.getMed6Caution()).borderWidth(1).build())
+                        .backgroundColor(Color.WHITE)
+                        .textColor(Color.BLACK)
+                        .font(PDType1Font.TIMES_ROMAN)
+                        .fontSize(10)
+                        .horizontalAlignment(CENTER)
+                        .build());
+                heightCounter = heightCounter - 24;
+                parallelCounter = parallelCounter - 48;
+            }
+
+            Table table = tableBuilder.build();
+
+            TableDrawer tableDrawer = TableDrawer.builder()
+                    .contentStream(contentStream)
+                    .startX(50)
+                    .startY(heightCounter)
+                    .table(table)
+                    .build();
+
+            contentStream.endText();
+            tableDrawer.draw();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return parallelCounter;
+    }
+
+    public float addExaminations(PDPageContentStream contentStream, Visit visit, float heightCounter) {
+        try {
+            contentStream.beginText();
+            contentStream.setLeading(17f);
+            contentStream.newLineAtOffset(50, heightCounter);
+            contentStream.setFont(PDType1Font.TIMES_BOLD, 12);
+            contentStream.showText("Pathological Examinations: ");
+            contentStream.setFont(PDType1Font.TIMES_ROMAN, 12);
+            contentStream.newLine();
+            heightCounter = heightCounter - 12;
+
+            String pathology = visit.getExaminationsPrescribed();
+            List<String> pathologyList = Arrays.asList(pathology.split(","));
+
+            if (visit.getExaminationsPrescribed().equals("")) {
+                contentStream.showText("no pathological examinations");
+                contentStream.newLine();
+                heightCounter = heightCounter - 12;
+            } else {
+                int index = 1;
+                for (String p : pathologyList) {
+                    contentStream.showText((index++) + "." + p);
+                    contentStream.newLine();
+                    heightCounter = heightCounter - 12;
+                }
+            }
+
+            contentStream.newLine();
+
+            contentStream.setFont(PDType1Font.TIMES_BOLD, 12);
+            contentStream.showText("Radiology Examinations: ");
+            contentStream.setFont(PDType1Font.TIMES_ROMAN, 12);
+            contentStream.newLine();
+            heightCounter = heightCounter - 12;
+
+            String radiology = visit.getRadiologyPrescribed();
+            List<String> radiologyList = Arrays.asList(radiology.split(","));
+
+            if (visit.getRadiologyPrescribed().equals("")) {
+                contentStream.showText("no radiology examinations");
+                contentStream.newLine();
+                heightCounter = heightCounter - 12;
+            } else {
+                int index = 1;
+                for (String r : radiologyList) {
+                    contentStream.showText((index++) + "." + r);
+                    contentStream.newLine();
+                    heightCounter = heightCounter - 12;
+                }
+            }
+            contentStream.newLine();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return heightCounter;
+
+    }
+
+    public void addFooter(PDPageContentStream contentStream, Visit visit) {
+        try {
+            contentStream.showText("Advisory: " + visit.getAdvisory());
+            contentStream.newLine();
+            contentStream.newLine();
+            if (visit.getNextVisitDate() != null) {
+                String date = visit.getNextVisitDate().toString().substring(0, 10);
+                String formattedDate = DateUtil.format(date);
+                contentStream.showText("Next Visit Date: " + formattedDate);
+                contentStream.newLine();
+                contentStream.newLine();
+            }
+            contentStream.setFont(PDType1Font.TIMES_BOLD, 12);
+            contentStream.showText("Note: Check your contact details & Aadhaar details. If any change or variance please advise.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
